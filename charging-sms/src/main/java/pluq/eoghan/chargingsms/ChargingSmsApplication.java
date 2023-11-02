d346663429a58dccee565deda1ff875f5f3c4228
Eoghan <eoghan.rainey@gmail.com>
2023-11-02 21:35:14 +0100

    first commit

---
+++ charging-sms/src/main/java/pluq/eoghan/chargingsms/ChargingSmsApplication.java
@@ -0,0 +1,13 @@
+package pluq.eoghan.chargingsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargingSmsApplication.class, args);
+	}
+
+}
---
+++ charging-sms/src/test/java/pluq/eoghan/chargingsms/ChargingSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pluq.eoghan.chargingsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
